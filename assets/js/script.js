e4da7fdafbbbc89d187b142525db2f3654ce0100
AnthonyTurijan <anthonyturijan@gmail.com>
2021-12-07 21:45:55 -0800

    meow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 // TODO: Declare any global variables we need
-
+let headsRolls = 0
+let tailsRolls = 0
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    document.getElementById('flip').addEventListener('click', function () {
+        let flippedHeads = Math.random() < 0.5
+        if (flippedHeads) {
+           document.getElementById('penny-image').src= 'assets/images/penny-heads.jpg'
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+            headsRolls += 1
+        }
+        else {
+            document.getElementById('penny-image').src= 'assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+            tailsRolls += 1
+        }
+    } )
+
+    
 
     // TODO: Add event listener and handler for flip and clear buttons
 
@@ -25,3 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
+
+
+
