00f91dc717d71a5ec2000b2107dea20a94677fce
AnthonyTurijan <anthonyturijan@gmail.com>
2021-12-07 21:55:01 -0800

    meow 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
            document.getElementById('penny-image').src= 'assets/images/penny-heads.jpg'
             document.getElementById('message').textContent = 'You Flipped Heads!'
             headsRolls += 1
+            document.getElementById('heads').textContent = headsRolls
+            let total = headsRolls + tailsRolls
+            document.getElementById('heads-percent').textContent = percentHeads + '%'
         }
         else {
             document.getElementById('penny-image').src= 'assets/images/penny-tails.jpg'
             document.getElementById('message').textContent = 'You Flipped Tails!'
             tailsRolls += 1
+            document.getElementById('tails').textContent = tailsRolls
+            let total = headsRolls + tailsRolls
+            document.getElementById('tails-percent').textContent = percentTails + '%'
         }
     } )
-
-    
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    if (total > 0) {
+        // Calculate percentage of heads and tails
+        percentHeads = Math.round((headsRolls / total) * 100)
+        percentTails = Math.round((tailsRolls / total) * 100)
+    }
 })
 
 
